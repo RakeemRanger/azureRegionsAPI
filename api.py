cf62826727997288ec4e83e61fe027242b487bc5
RakeemRanger <84100634+RakeemRanger@users.noreply.github.com>
2024-05-14 14:43:44 -0400

    Update api.py

--- api.py
+++ api.py
@@ -1,30 +1,56 @@
 import json
+import os
 
-from flask import Flask, request, jsonify
+from flask import Flask, request
 from flask_restful import Api, Resource
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.subscription import SubscriptionClient
+
+from CONSTANTS import AZURE_SUBSCRIPTION_ID
+
 
 app = Flask(__name__)
 api = Api(app)
 
-'''
-Get Azure Region Geographic data from a json file
-Source: Az CLI
-"az account list-locations --query '[].{name:name, latitude:metadata.latitude, longitude:metadata.longitude}' > azure_geos.json"
-Note: you can run a cronjob or jenkins job, etc... to run this command periodically to update deprecated and or added regions
-'''
-with open('azure_geos.json') as file:
-    app_json = json.loads(file.read())
 
+def azure_sub_locations():
+    '''
+    In a production environment, this function may live in a separate file 
+    (best practice is to store all of your client connections in a separate file and or folder.
+    this way you enable source control, versioning, collaboration, and security best practices). 
+    This function is used to get the Azure regions and their respective latitude and longitude.
+    I am storing my subscription id in an CONSTANTS file which calls a env variable for security purposes.
+    you can create a environment variable by running the below command in your terminal:
+    linux: > export AZURE_SUBSCRIPTION_ID="your_subscription_id"
+    windows: > $AZURE_SUBSCRIPTION_ID "your_subscription_id"
+    '''
+    creds = DefaultAzureCredential()
+    sub_client = SubscriptionClient(creds,)
+    subscription = sub_client.subscriptions.list_locations(AZURE_SUBSCRIPTION_ID)
+    empty = []
+    for location in subscription:
+        empty.append({"name" : f'{location.name}', "latitude" : f'{location.latitude}', "longitude" : f'{location.longitude}'})
+    api_json = json.loads(json.dumps(empty))
+    return api_json
         
+ 
 class AzureRegions(Resource):
     def get(self, ):
+        '''
+        returns a list of Azure regions and their respective latitude and longitude.
+        '''
         if request.method == 'GET':
-            return jsonify(app_json)
+            app_json = azure_sub_locations()
+            return app_json
 
 
 class AzureSingleRegion(Resource):
      def get(self, location):
+        '''
+        returns a single Azure region and its respective latitude and longitude.
+        '''
         request.data = location
+        app_json = azure_sub_locations()
         if request.method == 'GET':
             output_dict = [x for x in app_json if x['name'] == request.data]
             return output_dict
